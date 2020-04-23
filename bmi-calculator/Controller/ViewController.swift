59a7e661acc87df90fac3e65a373eac6aefd0a91
mikedilulo <mikedilulo2020@gmail.com>
2020-04-23 11:08:42 -0600

    Add Weight IBOutlet

--- bmi-calculator/Controller/ViewController.swift
+++ bmi-calculator/Controller/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var heightOutlet: UILabel!
+    @IBOutlet weak var weightOutlet: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
