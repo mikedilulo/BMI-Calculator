b6926b7cd6253deea7d28565441bab15611c5ac8
mikedilulo <mikedilulo2020@gmail.com>
2020-04-23 11:16:59 -0600

    Remove Outlets for BMI Calc

--- bmi-calculator/Controller/ViewController.swift
+++ bmi-calculator/Controller/ViewController.swift
@@ -9,16 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var heightOutlet: UILabel!
-    @IBOutlet weak var weightOutlet: UILabel!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    @IBAction func calculateBmiButton(_ sender: UIButton) {
-    }
-    
+
 
 }
 
