bf1121a8cf9152b78f4c78728fac98db1019e7f9
mikedilulo <mikedilulo2020@gmail.com>
2020-04-23 11:21:45 -0600

    Add UISlider IBAction Slider

--- bmi-calculator/Controller/ViewController.swift
+++ bmi-calculator/Controller/ViewController.swift
@@ -16,7 +16,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func sliderValueAction(_ sender: UISlider) {
+    }
+    
 
 }
 
