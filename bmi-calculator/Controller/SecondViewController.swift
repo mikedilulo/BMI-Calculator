587cae5222aacd6629d847fffda36ddbcf78d7c3
mikedilulo <mikedilulo2020@gmail.com>
2020-04-27 15:15:42 -0600

    Add SecondViewController file

---
+++ bmi-calculator/Controller/SecondViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SecondViewController.swift
+//  bmi-calculator
+//
+//  Created by Mike Di’ Lulo on 4/27/20.
+//  Copyright © 2020 Mike Di’ Lulo. All rights reserved.
+//
+
+import Foundation
