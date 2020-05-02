86b32cf773b3971316838adc626dfc7510860f31
mikedilulo <mikedilulo2020@gmail.com>
2020-05-02 11:09:19 -0600

    Add Cocoa Touch File

---
+++ ResultsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultsViewController.swift
+//  bmi-calculator
+//
+//  Created by Mike Di’ Lulo on 5/2/20.
+//  Copyright © 2020 Mike Di’ Lulo. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
