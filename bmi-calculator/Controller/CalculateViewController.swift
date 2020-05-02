29f5bf0806a2a6f8f1246327f9706edb21fa5af6
mikedilulo <mikedilulo2020@gmail.com>
2020-05-02 11:18:10 -0600

    Rename First Controller

--- bmi-calculator/Controller/CalculateViewController.swift
+++ bmi-calculator/Controller/CalculateViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CalculateViewController: UIViewController {
     @IBOutlet weak var heightOutlet: UILabel!
     @IBOutlet weak var weightOutlet: UILabel!
     
