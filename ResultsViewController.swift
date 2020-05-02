609c6f6c46407ec665b919a11d3ced206eba3ef6
mikedilulo <mikedilulo2020@gmail.com>
2020-05-02 11:16:33 -0600

    Add IBAction - Recalculate

--- ResultsViewController.swift
+++ ResultsViewController.swift
@@ -19,7 +19,9 @@ class ResultsViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+    
     /*
     // MARK: - Navigation
 
