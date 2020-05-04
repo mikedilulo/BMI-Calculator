3f4e6d80e37d1a8e9b47020f0d7116432a941e60
mikedilulo <mikedilulo2020@gmail.com>
2020-05-04 15:44:20 -0600

    remove bmiValue print statement

--- bmi-calculator/Controller/CalculateViewController.swift
+++ bmi-calculator/Controller/CalculateViewController.swift
@@ -39,7 +39,7 @@ class CalculateViewController: UIViewController {
         // pow is used for exponents
 //        let bmi = weight / pow(height, 2)
         let bmi = weight / (height * weight)
-        print(bmi)
+        
         
         self.performSegue(withIdentifier: "goToResults", sender: self)
 
