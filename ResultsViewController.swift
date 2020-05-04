9e8bdc0b6f589dba0092f072c4d793ce1045a1bb
mikedilulo <mikedilulo2020@gmail.com>
2020-05-04 15:47:08 -0600

    bmiLabel.text update in viewDidLoad( )

--- ResultsViewController.swift
+++ ResultsViewController.swift
@@ -20,7 +20,7 @@ class ResultsViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        
+        bmiLabel.text = bmiValue
     }
     
     @IBAction func recalculatePressed(_ sender: UIButton) {
